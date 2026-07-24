2e3f7415f6704229fef39356e4df151fe584365f
pstN <ozane.faubert@icloud.com>
2026-07-23 20:18:24 -0400

    fix(fgame): retain spectator target through death

--- code/fgame/player.cpp
+++ code/fgame/player.cpp
@@ -4806,7 +4806,7 @@ void Player::Think(void)
                     if (!ent->inuse || !ent->entity) {
                         // Invalid spectate entity
                         SetPlayerSpectateRandom();
-                    } else if (ent->entity->deadflag >= DEAD_DEAD || static_cast<Player *>(ent->entity)->IsSpectator()
+                    } else if (static_cast<Player *>(ent->entity)->IsSpectator()
                                || !IsValidSpectatePlayer(static_cast<Player *>(ent->entity))) {
                         SetPlayerSpectateRandom();
                     }
@@ -4831,7 +4831,7 @@ void Player::Think(void)
                     if (!ent->inuse || !ent->entity) {
                         // Invalid spectate entity
                         SetPlayerSpectateRandom();
-                    } else if (ent->entity->deadflag >= DEAD_DEAD || static_cast<Player *>(ent->entity)->IsSpectator()
+                    } else if (static_cast<Player *>(ent->entity)->IsSpectator()
                                || !IsValidSpectatePlayer(static_cast<Player *>(ent->entity))) {
                         SetPlayerSpectateRandom();
                     } else if (g_gametype->integer >= GT_TEAM && GetTeam() > TEAM_FREEFORALL
